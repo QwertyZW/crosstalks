4557f3f54f0b17a77fbe734cfb5e69b2ee6a6739
zaid Alkhishman <zaid Alkhishman>
2017-04-23 13:11:51 +0000

    add lexer test cases

--- test/lexer/lexer_test.c
+++ test/lexer/lexer_test.c
@@ -1,1 +1,79 @@
-// TODO: 
+#include <stdarg.h>
+#include <stddef.h>
+#include <setjmp.h>
+#include <cmocka.h>
+
+#include "lexer.h"
+/* A test case that does nothing and succeeds. */
+static void beginsWith_test_true(void **state) {
+    (void) state; /* unused */
+
+    int x = beginsWith("ifzzzz", "if");
+    assert_int_equal(x, 2);
+}
+
+/* A test case that does nothing and succeeds. */
+static void beginsWith_test_false(void **state) {
+    (void) state; /* unused */
+
+    int x = beginsWith("iifzzzz", "if");
+    assert_int_equal(x, 0);
+}
+
+static void beginsWith_test_param1Null(void **state) {
+    (void) state; /* unused */
+
+    int x = beginsWith(0, "if");
+    assert_int_equal(x, 0);
+}
+
+static void beginsWith_test_param2Null(void **state) {
+    (void) state; /* unused */
+
+    int x = beginsWith("iifzzzz", 0);
+    assert_int_equal(x, 0);
+}
+
+static void beginsWith_test_paramAllNull(void **state) {
+    (void) state; /* unused */
+
+    int x = beginsWith(0, 0);
+    assert_int_equal(x, 0);
+}
+
+
+static void beginsWith_test_param1Empty(void **state) {
+    (void) state; /* unused */
+
+    int x = beginsWith("", "if");
+    assert_int_equal(x, 0);
+}
+
+static void beginsWith_test_param2Empty(void **state) {
+    (void) state; /* unused */
+
+    int x = beginsWith("iifzzzz", "");
+    assert_int_equal(x, 0);
+}
+
+static void beginsWith_test_paramAllEmpty(void **state) {
+    (void) state; /* unused */
+
+    int x = beginsWith("", "");
+    assert_int_equal(x, 0);
+}
+
+int main(void) {
+    const struct CMUnitTest tests[] = {
+        cmocka_unit_test(beginsWith_test_true),
+        cmocka_unit_test(beginsWith_test_false),
+        cmocka_unit_test(beginsWith_test_param1Null),
+        cmocka_unit_test(beginsWith_test_param2Null),
+        cmocka_unit_test(beginsWith_test_paramAllNull),
+        cmocka_unit_test(beginsWith_test_param1Empty),
+        cmocka_unit_test(beginsWith_test_param2Empty),
+        cmocka_unit_test(beginsWith_test_paramAllEmpty)
+    };
+
+    return cmocka_run_group_tests(tests, NULL, NULL);
+}
